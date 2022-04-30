57005e18741651eea21eda140117922c1ec88a0d
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2022-04-30 18:25:55 +0300

    update and recordapi

--- routers/controllers/record.js
+++ routers/controllers/record.js
@@ -37,17 +37,32 @@ const getUserRecords = (req, res) => {
 		});
 };
 
-const addNewRecord = (req, res) => {
-	const record = new recordModel(req.body);
+const addNewRecord = async (req, res) => {
+	const { user, amountOfTurn } = req.body;
 
-	record
-		.save()
-		.then((result) => {
-			res.status(201).json(result);
-		})
-		.catch((err) => {
-			res.status(400).json(err);
-		});
+	const found = await recordModel.findOne({ user });
+
+	if (found) {
+		recordModel
+			.findOneAndUpdate({ user }, { amountOfTurn }, { new: true })
+			.then((result) => {
+				res.status(200).json(result);
+			})
+			.catch((err) => {
+				res.status(400).json(err);
+			});
+	} else {
+		const record = new recordModel(req.body);
+
+		record
+			.save()
+			.then((result) => {
+				res.status(201).json(result);
+			})
+			.catch((err) => {
+				res.status(400).json(err);
+			});
+	}
 };
 
 module.exports = {
